10952603bbafbcfdb50545c9a9dec9641c2df4ed
shafayatGit <shafayathossain.drmc@gmail.com>
2025-02-18 14:27:12 +0600

    4 done

--- prob_4.js
+++ prob_4.js
@@ -1,1 +1,19 @@
-function fs(){}
+function validProposal(input1, input2){
+    
+    if( typeof(input1) === 'object' && typeof(input2) === 'object'){
+        if((input1['gender'] !== input2['gender']) && (Math.abs(input1['age']-input2['age'])<=7)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return 'Invalid'
+    }
+    
+}
+
+let willMarry = validProposal({ name: "mitu", gender: "male", age: 32 }, "Mizan" )
+
+console.log(willMarry)
