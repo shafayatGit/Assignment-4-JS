3d3b2957540ae41009f4ab5448c403a7f039a30d
shafayatGit <shafayathossain.drmc@gmail.com>
2025-02-18 10:30:12 +0600

    push

---
+++ Prob_1.js
@@ -0,0 +1,12 @@
+function calculateVAT(amount) {
+    let vat = amount * 0.075;
+
+    if(typeof(amount) !== 'number' || amount < 0){
+        return 'Invalid';
+    }
+    
+    
+    return vat;
+}
+let result = calculateVAT('foo');
+console.log(result)
